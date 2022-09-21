41271d78790579ca5b05c4fe1529df391ce9bf6b
Blake Nicholson <90578110+b-nicholson@users.noreply.github.com>
2022-09-21 22:44:02 +0300

    Update

--- Blake Dev Tools.extension/Blakes Tab.tab/In Development.panel/insertGroupatPoint.pushbutton/script.py
+++ Blake Dev Tools.extension/Blakes Tab.tab/In Development.panel/insertGroupatPoint.pushbutton/script.py
@@ -2,6 +2,7 @@
 from math import sin, cos
 import Autodesk.Revit.DB as DB
 from pyrevit import forms
+from pyrevit.forms import ProgressBar
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
@@ -51,32 +52,36 @@ def place_group_at_family_instance_pt(group_type_list, family_type_selection):
     locations = []
     rotations = []
     mirrored = []
+    max_value = len(jig_collector)
 
     for jig in jig_collector:
         locations.append(jig.Location.Point)
         rotations.append(jig.Location.Rotation)
         mirrored.append(jig.Mirrored)
 
-    t = DB.Transaction(doc, "Place Groups at Point")
-    t.Start()
-
-    for loc, angle, flip in zip(locations, rotations, mirrored):
-        new_group = doc.Create.PlaceGroup(loc, group_type)
-        axis_line = DB.Line.CreateBound(loc, loc + DB.XYZ.BasisZ)
-        DB.ElementTransformUtils.RotateElement(doc, new_group.Id, axis_line, angle)
-        if flip:
-            ''' If the instance is mirrored, we need to create a plane to mirror the group, 
-                aligned with the group's rotation'''
-            origin = (loc.X, loc.Y)
-            point = (loc.X+1, loc.Y)
-            new_pt_x = origin[0] + cos(angle) * (point[0] - origin[0]) - sin(angle) * (point[1] - origin[1])
-            new_pt_y = origin[1] + sin(angle) * (point[0] - origin[0]) + cos(angle) * (point[1] - origin[1])
-            rotated_pt = DB.XYZ(new_pt_x, new_pt_y, loc.Z)
-            axis_plane = DB.Plane.CreateByThreePoints(rotated_pt, loc, loc + DB.XYZ.BasisZ)
-            DB.ElementTransformUtils.MirrorElement(doc, new_group.Id, axis_plane)
-            # Revit forces you to make a copy when mirroring, so we need to delete the old instance
-            doc.Delete(new_group.Id)
-    t.Commit()
+    with ProgressBar(title='Placing Groups... ({value} of {max_value})', cancellable=True) as pb:
+        i = 0
+        for loc, angle, flip in zip(locations, rotations, mirrored):
+            new_group = doc.Create.PlaceGroup(loc, group_type)
+            axis_line = DB.Line.CreateBound(loc, loc + DB.XYZ.BasisZ)
+            DB.ElementTransformUtils.RotateElement(doc, new_group.Id, axis_line, angle)
+            if flip:
+                ''' If the instance is mirrored, we need to create a plane to mirror the group, 
+                    aligned with the group's rotation'''
+                origin = (loc.X, loc.Y)
+                point = (loc.X+1, loc.Y)
+                new_pt_x = origin[0] + cos(angle) * (point[0] - origin[0]) - sin(angle) * (point[1] - origin[1])
+                new_pt_y = origin[1] + sin(angle) * (point[0] - origin[0]) + cos(angle) * (point[1] - origin[1])
+                rotated_pt = DB.XYZ(new_pt_x, new_pt_y, loc.Z)
+                axis_plane = DB.Plane.CreateByThreePoints(rotated_pt, loc, loc + DB.XYZ.BasisZ)
+                DB.ElementTransformUtils.MirrorElement(doc, new_group.Id, axis_plane)
+                # Revit forces you to make a copy when mirroring, so we need to delete the old instance
+                doc.Delete(new_group.Id)
+            if pb.cancelled:
+                break
+            else:
+                pb.update_progress(i, max_value)
+            i += 1
 
 
 jig_type_collector = DB.FilteredElementCollector(doc) \
@@ -91,8 +96,10 @@ group_type_collector = DB.FilteredElementCollector(doc) \
 
 type_selection = item_selector(jig_type_collector).Id
 
+t = DB.Transaction(doc, "Place Groups at Point")
+t.Start()
 place_group_at_family_instance_pt(group_type_collector, type_selection)
-
+t.Commit()
 
 '''mirroring needs to be the very last step, since the MirrorElement method does not return the element ids!?!?
 must make the axis_plane creation's 2nd point be rotated by the family's rotation
