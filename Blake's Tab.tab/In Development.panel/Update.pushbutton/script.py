abc7dbb1720a6196528c6d5acbd5ca1a269b31ae
Blake Nicholson <90578110+b-nicholson@users.noreply.github.com>
2022-10-11 12:26:26 -0400

    Update

--- Blake's Tab.tab/In Development.panel/Update.pushbutton/script.py
+++ Blake's Tab.tab/In Development.panel/Update.pushbutton/script.py
@@ -1,5 +1,16 @@
 # -*- coding: UTF-8 -*-
-
+from pyrevit import script
+from pyrevit.loader import sessionmgr
+from pyrevit.loader import sessioninfo
 import os
 
-os.system('cmd /c "pyrevit extensions update --all --debug"')
+os.system('cmd /c "pyrevit extensions update --all"')
+
+logger = script.get_logger()
+results = script.get_results()
+
+# re-load pyrevit session.
+logger.info('Reloading....')
+sessionmgr.reload_pyrevit()
+
+results.newsession = sessioninfo.get_session_uuid()
