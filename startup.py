73c4a2c18b939716da2e6cc5491fac5f261da3c4
Blake Nicholson <90578110+b-nicholson@users.noreply.github.com>
2022-10-30 22:07:05 -0400

    what

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ from pyrevit.userconfig import user_config
 try:
     warning_colour_str = user_config.swc_timer.get_option("warning_colour", "#FF0000")
 except AttributeError:
-    user_config.add_section("swc_timer")
     from SyncTimer.welcome import print_welcome
     print_welcome()
+    user_config.add_section("swc_timer")
 
