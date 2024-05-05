0312ac273d1b38a0a66564d218080e699810e793
Blake Nicholson <90578110+b-nicholson@users.noreply.github.com>
2024-05-05 12:24:52 -0400

    Update

--- lib/SyncTimer/welcome.py
+++ lib/SyncTimer/welcome.py
@@ -1,40 +1,42 @@
 def print_welcome():
 
-    from pyrevit import script
+    # from pyrevit import script
+    import webbrowser
+    webbrowser.open("https://github.com/b-nicholson/RevitUtils/blob/master/lib/SyncTimer/welcome.html")
 
-    output = script.get_output()
-    output.print_html("<h1><strong>Welcome!</strong></h1>\
-    <p>&nbsp;</p>\
-    <p>Thanks for installing my extension. It is currently, and probably will forever be, a work in progress. If something goes wrong, or if you have any suggestions please let me know.</p>\
-    <p>It installs a new Tab at the top of the Revit Ribbon called <em>pyLoop</em>Inside are a variety of small tools that I will do my best to keep adding to.</p>\
-    <p>&nbsp;</p>\
-    <p>There are some utilities that work in the background which modify the way Revit operates, but are not immediately obvious that they&#39;re running. They are explained below:</p>\
-    <p>&nbsp;</p>\
-    <p><strong>Synchronize With Central Timer</strong></p>\
-    <ul>\
-    <li>\
-    This plugin will gradually change the colour of your <em>Application Ribbon</em> if you have not synced a workshared document in a user-specified duration. If you exceed the time, it will automatically switch your active tab to <em>Collaborate</em>, and highlight the <em>Synchronize</em> panel.\
-    </li>\
-    <li>\
-    You can adjust the timing and colour settings within the <em>Sync With Central Timer Settings</em> button inside of <em>Blake&#39;s Tab.</em>\
-    </li>\
-    </ul>\
-    <p><strong>Import CAD</strong></p>\
-    <ul>\
-        <li>If you try to import a CAD file inside of a project (not a family) it will produce a warning, but still allow you to do it if you REALLY need to. (but why!?)</li>\
-    </ul>\
-    <p><strong>Model In Place</strong></p>\
-    <ul>\
-        <li>If you try to create a Model in Place Component it will produce a warning, but still allow you to do it. This is a tool for mindfulness.</li>\
-    </ul>\
-        <p><strong>Printing With Closed Worksets</strong></p>\
-    <ul>\
-        <li>If you go to print with any worksets closed, it will warn you before you start the process.</li>\
-    </ul>\
-    <p><strong>Ungroup Groups</strong></p>\
-    <ul>\
-        <li>I dont know about you, but I misclick this a lot. No idea who thought having it beside Edit Group in the UI was a good idea. Not noticing it was accidentally ungrouped has bad consequences. The plugin will give you a warning before ungrouping.</li>\
-    </ul>\
-    <p><strong>To make this pop-up stop appearing, Enter the Sync with Central Timer Settings and save your desired configuration.</strong> (This is deliberately obnoxious, sorry)</p>\
-    <p>&nbsp;</p>\
-    ")
+    # output = script.get_output()
+    # output.print_html("<h1><strong>Welcome!</strong></h1>\
+    # <p>&nbsp;</p>\
+    # <p>Thanks for installing my extension. It is currently, and probably will forever be, a work in progress. If something goes wrong, or if you have any suggestions please let me know.</p>\
+    # <p>It installs a new Tab at the top of the Revit Ribbon called <em>pyLoop</em>Inside are a variety of small tools that I will do my best to keep adding to.</p>\
+    # <p>&nbsp;</p>\
+    # <p>There are some utilities that work in the background which modify the way Revit operates, but are not immediately obvious that they&#39;re running. They are explained below:</p>\
+    # <p>&nbsp;</p>\
+    # <p><strong>Synchronize With Central Timer</strong></p>\
+    # <ul>\
+    # <li>\
+    # This plugin will gradually change the colour of your <em>Application Ribbon</em> if you have not synced a workshared document in a user-specified duration. If you exceed the time, it will automatically switch your active tab to <em>Collaborate</em>, and highlight the <em>Synchronize</em> panel.\
+    # </li>\
+    # <li>\
+    # You can adjust the timing and colour settings within the <em>Sync With Central Timer Settings</em> button inside of <em>Blake&#39;s Tab.</em>\
+    # </li>\
+    # </ul>\
+    # <p><strong>Import CAD</strong></p>\
+    # <ul>\
+    #     <li>If you try to import a CAD file inside of a project (not a family) it will produce a warning, but still allow you to do it if you REALLY need to. (but why!?)</li>\
+    # </ul>\
+    # <p><strong>Model In Place</strong></p>\
+    # <ul>\
+    #     <li>If you try to create a Model in Place Component it will produce a warning, but still allow you to do it. This is a tool for mindfulness.</li>\
+    # </ul>\
+    #     <p><strong>Printing With Closed Worksets</strong></p>\
+    # <ul>\
+    #     <li>If you go to print with any worksets closed, it will warn you before you start the process.</li>\
+    # </ul>\
+    # <p><strong>Ungroup Groups</strong></p>\
+    # <ul>\
+    #     <li>I dont know about you, but I misclick this a lot. No idea who thought having it beside Edit Group in the UI was a good idea. Not noticing it was accidentally ungrouped has bad consequences. The plugin will give you a warning before ungrouping.</li>\
+    # </ul>\
+    # <p><strong>To make this pop-up stop appearing, Enter the Sync with Central Timer Settings and save your desired configuration.</strong> (This is deliberately obnoxious, sorry)</p>\
+    # <p>&nbsp;</p>\
+    # ")
